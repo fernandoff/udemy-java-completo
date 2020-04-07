60b7f58b9cc96a3e0671e59366a8b08a085eeb34
fernando <fernando.cua@gmail.com>
2020-04-07 16:52:20 -0300

    Java ODBC

--- lambda4-java/src/application/Program.java
+++ lambda4-java/src/application/Program.java
@@ -17,8 +17,17 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+		// List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());		
+		// names.forEach(System.out::println);
 		
-		names.forEach(System.out::println);
+		// list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList()).forEach(System.out::println);
+		
+		list.stream()
+			.map(
+					p -> 
+						new Product(p.getName().toUpperCase(), p.getPrice() * 1.1)
+			)
+			.collect(Collectors.toList())
+			.forEach(System.out::println);
 	}
 }
