b7838e3bf36a2ed6a3fb0b4badef35db0144164d
fernando <fernando.cua@gmail.com>
2020-04-08 16:41:15 -0300

    Spring boot

---
+++ springbasico/src/main/java/com/spring/course/SpringbasicoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbasicoApplication.class, args);
+	}
+
+}
